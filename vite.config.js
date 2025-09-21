a220f99978d015111d062218d8d3fb6237250dbb
FutureTL <futuretechnologistics21@gmail.com>
2025-09-21 14:53:16 +0530

    Link for backend on render updated

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server:{
     proxy:{
-      '/api': 'http://localhost:3000',
+      '/api': 'https://blogapp-1-gn6j.onrender.com',
     }
   },
   plugins: [
